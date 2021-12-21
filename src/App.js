3e67d75e4ca4317850040293ea7ad7c0adff70e4
Wesley Bezerra Rocha <wesley.rocha@vh.com.br>
2021-12-21 19:32:10 -0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import AboutUs from './components/pages/AboutUs'
+import Contact from './components/pages/Contact'
+
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Início</Link>
+        <Link to="/services">Serviços</Link>
+        <Link to="/about-us">Sobre nós</Link>
+        <Link to="/contact">Contato</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="services" element={<Services/>} />
+          <Route path="about-us" element={<AboutUs/>} />
+          <Route path="contact" element={<Contact/>} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
