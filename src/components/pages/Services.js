8d8796aeec174155e0c7b1f26a5f375b47bca0c1
Wesley Bezerra Rocha <wesley.rocha@vh.com.br>
2021-12-27 15:47:30 -0300

    Services update

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,7 +1,29 @@
+import styles from './Services.module.css'
+import { FaSketch, FaDraftingCompass, FaTools } from 'react-icons/fa'
+
 function Services() {
     return (
-        <div>
-            <h1>Serviços</h1>
+        <div className={styles.services}>
+            <h2>Conheça os nossos serviços</h2>
+            <p>Bem-vindo a WBR, a primeira e mais completa plataforma de ensino de Mandarim gratuíto do Brasil!</p>
+            <hr></hr>
+            <ul className={styles.grid}>
+                <li>
+                    <FaSketch className={styles.svg}/>
+                    <h4>Material audio-visual</h4>
+                    <p>Aulas exclusívas de Mandarim desde básico até o avançado</p>
+                </li>
+                <li>
+                    <FaDraftingCompass className={styles.svg}/>
+                    <h4>Tradução de conteúdo em Português</h4>
+                    <p>Acompanhe a nossa apostila de conteúdo totalmente em Português</p>
+                </li>
+                <li>
+                    <FaTools className={styles.svg}/>
+                    <h4>Simulados do HSK e HSKK (Teste de Proficiência em Chinês)</h4>
+                    <p>Faça simulados online e prepare-se para os testes de Proficiência</p>
+                </li>
+            </ul>
         </div>
     )
 }
